4548f7016ab93ae178425265d51f4b51f90915b3
sergey radionov <rsatom@gmail.com>
2022-06-11 23:01:23 +0700

    config file support added

--- Server.cpp
+++ Server.cpp
@@ -3,9 +3,14 @@
 
 #include <fmt/format.h>
 
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_sinks.h>
+
 #include "CxxPtr/GlibPtr.h"
 #include "CxxPtr/GstRtspServerPtr.h"
+#include "CxxPtr/libconfigDestroy.h"
 
+#include "ConfigHelpers.h"
 
 #define BARS  "bars"
 #define WHITE "white"
@@ -14,9 +19,67 @@
 #define GREEN "green"
 #define BLUE  "blue"
 
+static std::unique_ptr<spdlog::logger> Log;
+
+struct Config {
+    uint16_t port = 8554;
+};
+
+static void InitLogger() {
+    spdlog::sink_ptr sink = std::make_shared<spdlog::sinks::stdout_sink_st>();
+
+    Log = std::make_unique<spdlog::logger>("rtsp-test-server", sink);
+
+    Log->set_level(spdlog::level::info);
+}
+
+static void LoadConfig(Config* config)
+{
+    const std::deque<std::string> configDirs = ::ConfigDirs();
+    if(configDirs.empty())
+        return;
+
+    Config loadedConfig = *config;
+
+    for(const std::string& configDir: configDirs) {
+        const std::string configFile = configDir + "/rtsp-test-server.conf";
+        if(!g_file_test(configFile.c_str(), G_FILE_TEST_IS_REGULAR)) {
+            continue;
+        }
+
+        config_t config;
+        config_init(&config);
+        ConfigDestroy ConfigDestroy(&config);
+
+        Log->error("Loading config \"{}\"", configFile);
+        if(!config_read_file(&config, configFile.c_str())) {
+            Log->error("Fail load config. {}. {}:{}",
+                config_error_text(&config),
+                configFile,
+                config_error_line(&config));
+            return;
+        }
+
+        int port = 0;
+        if(CONFIG_TRUE == config_lookup_int(&config, "port", &port)) {
+            if(port > 0 && port < 65535 ) {
+                loadedConfig.port = port;
+            } else {
+                Log->error("Invalid port value");
+            }
+        }
+    }
+
+    *config = loadedConfig;
+}
 
 int main(int argc, char *argv[])
 {
+    InitLogger();
+
+    Config config;
+    LoadConfig(&config);
+
     const std::string h264PipelineTemplate =
         "( videotestsrc pattern={} ! "
         "x264enc ! video/x-h264, profile=baseline ! "
@@ -42,16 +105,18 @@ int main(int argc, char *argv[])
     GstRTSPServerPtr staticServer(gst_rtsp_server_new());
     GstRTSPServer* server = staticServer.get();
     if(!server) {
-        GST_ERROR("Fail to create rtsp server");
+        Log->error("Fail to create rtsp server");
         return -1;
     }
 
     GstRTSPMountPointsPtr mountPointsPtr(gst_rtsp_mount_points_new());
     GstRTSPMountPoints* mountPoints = mountPointsPtr.get();
     if(!server) {
-        GST_ERROR("Fail to create mount points");
+        Log->error("Fail to create mount points");
         return -1;
     }
+
+    gst_rtsp_server_set_service(server, std::to_string(config.port).c_str());
 
     gst_rtsp_server_set_mount_points(server, mountPoints);
 
